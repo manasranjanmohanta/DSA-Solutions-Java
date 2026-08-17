651521aec72ee583eae521794a197ff893ad8cc5
Manas Ranjan Mohanta <manasranjanmohanta679@gmail.com>
2026-08-17 22:36:03 +0530

    Time: 2 ms (67.38%), Space: 85.1 MB (60.13%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,23 +1,47 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        int direction = 0;
+        boolean isMonotoneIncreasing = false;
+        boolean isMonotoneDecreasing = false;
 
         int n = nums.length;
         for (int i = 0; i < n - 1; i++) {
-            if(nums[i] < nums[i+1]){
-                if(direction == 0){
-                    direction = 1;
-                } else if (direction == -1){
-                    return false;
-                }
-            } else if (nums[i] > nums[i+1]){
-                if(direction == 0){
-                    direction = -1;
-                } else if (direction == 1){
-                    return false;
-                }
+            if (nums[i] < nums[i + 1]) {
+                isMonotoneIncreasing = true;
+            }
+            if (nums[i] > nums[i + 1]) {
+                isMonotoneDecreasing = true;
             }
         }
-    return true;
+
+        if (isMonotoneIncreasing == true && isMonotoneDecreasing == true) {
+            return false;
+        }
+        return true;
     }
 }
+
+// SOLUTION - 2 (BY DECIDING DIRECTION)
+
+// class Solution {
+//     public boolean isMonotonic(int[] nums) {
+//         int direction = 0;
+
+//         int n = nums.length;
+//         for (int i = 0; i < n - 1; i++) {
+//             if(nums[i] < nums[i+1]){
+//                 if(direction == 0){
+//                     direction = 1;
+//                 } else if (direction == -1){
+//                     return false;
+//                 }
+//             } else if (nums[i] > nums[i+1]){
+//                 if(direction == 0){
+//                     direction = -1;
+//                 } else if (direction == 1){
+//                     return false;
+//                 }
+//             }
+//         }
+//     return true;
+//     }
+// }
