032ecdb1f6b17ca36177905b997b91f8446b1d1f
Manas Ranjan Mohanta <manasranjanmohanta679@gmail.com>
2026-08-18 23:14:46 +0530

    Time: 40 ms (36.2%), Space: 46.8 MB (91.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,18 +4,18 @@ class Solution {
 
         // if the array length is exactly 2
         if (n == 2) {
-            return new int[]{0, 1};
+            return new int[] { 0, 1 };
         }
 
         // if the array length is larger than 2
         for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
                 if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
+                    return new int[] { i, j };
                 }
             }
         }
-        
-        return new int[0];
+
+        return new int[] {};
     }
 }
