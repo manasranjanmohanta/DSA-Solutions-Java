b651f4aa7382594a7ef0839bd42b03a5325becbf
Manas Ranjan Mohanta <manasranjanmohanta679@gmail.com>
2026-08-19 23:12:54 +0530

    Time: 1 ms (61.09%), Space: 43.2 MB (51.04%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,16 +1,22 @@
+// INTUTITION:
+// CREATE A RESULT STRING TO STORE THE COMMON PREFIX CHARACTER
+// SORT THE STRING ARRAY IN LEXICOGRAPHICALLY WHICH IS ALPHABETICALLY
+// IF THE ARRAY IS SORTED THAT MEANS WE NEED CHECK ONLY THE FIRST ELEMENT AND LAST ELEMENT CHARACTERS
+// IF THE CHARACTERS OF FIRST AND LAST ELEMENT DOESN'T MATCH THEN SIMPLE RETURN THE RESULT ARRAY
+// OTHERWISE APPEND THE CHARACTER IN THE RESULT ARRAY
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        StringBuilder ans = new StringBuilder();
+        StringBuilder result = new StringBuilder();
         Arrays.sort(strs);
         String firstElement = strs[0];
         String lastElement = strs[strs.length - 1];
 
         for (int i = 0; i < Math.min(firstElement.length(), lastElement.length()); i++) {
             if (firstElement.charAt(i) != lastElement.charAt(i)) {
-                return ans.toString();
+                return result.toString();
             }
-            ans.append(firstElement.charAt(i));
+            result.append(firstElement.charAt(i));
         }
-        return ans.toString();
+        return result.toString();
     }
 }
